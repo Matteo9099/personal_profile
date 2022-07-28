adc8db22288ff6002765cb7dcff6a429fbca2e65
Matteo Morganti <matteo99morganti@gmail.com>
2022-07-28 10:05:15 +0200

    components & assets

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="card pb-5" style="width: 18rem;">
+       <img :src="require(`../assets/img/${image}`)" :alt="title">
+        <div class="card-body ms_overflow_card">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">{{ content }}</p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props: ['slides', 'image', 'title', 'content', 'badge'],
+}
+</script>
+
+<style scoped lang="scss">
+img {
+    height: 200px;
+    object-fit: cover;
+}
+</style>
+
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar fixed-top navbar-light pt-4">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div>
+                <a href="/" class="name_mm navbar-brand text-white">Matteo Morganti</a>
+            </div>
+            <div>
+                <a class="navbar-brand text-white" href="https://github.com/Matteo9099"><i class="fa-brands fa-github"></i></a>
+                <a class="navbar-brand text-white px-3" href="https://www.linkedin.com/in/matteo-morganti-developer"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+
+</style>
